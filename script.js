589cb7c2ce50dd17130e02c17524e2612cb49c9e
John Whiles <johnwhiles@outlook.com>
2016-10-07 15:19:07 +0100

    added ability to repeat previous operations on equal press

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ var storedNumber;
 var workingNumber = '0';
 var operand;
 var prev = document.getElementById('prev').innerHTML;
+var equalRepeat = false;
+var numRepeat;
+var opRepeat;
+var resRepeat;
 
+function repeat(result, ope, repNum) {
+    storedNumber = result;
+    workingNumber = repNum;
+    operand = ope;
+    equalsPress();
+}
 
 function numberPress(num) {
     if ((/\./g).test(workingNumber) && num === '.') {
@@ -30,9 +40,13 @@ function clearPress() {
 
 function equalsPress() {
     if (storedNumber && workingNumber && operand) {
+        numRepeat = workingNumber;
+        opRepeat = operand;
         var result = operators[operand](parseFloat(storedNumber, 10), parseFloat(workingNumber, 10));
         var theCalculation = storedNumber + ' ' + symbols[operand] + ' ' + workingNumber + ' = ' + result + '<br>';
         storedNumber = result;
+        console.log(result, storedNumber);
+        resRepeat = result;
         workingNumber = '0';
         operand = null;
         //changes the storednumber to the result of our calculation, sets working number to 0, sets operator to null
@@ -40,6 +54,7 @@ function equalsPress() {
         console.log(theCalculation);
         console.log(prev);
         document.getElementById('prev').innerHTML = theCalculation + document.getElementById('prev').innerHTML;
+        equalRepeat = true;
     } else {
         storedNumber = workingNumber;
         workingNumber = '0';
@@ -93,15 +108,22 @@ function somefunction(e) {
     if (e.target !== e.currentTarget) {
         var clickedID = e.target.id;
         var clickedClass = e.target.className;
+        if (equalRepeat && clickedID === 'equal') {
+            repeat(resRepeat, opRepeat, numRepeat);
+            
+        } else if (clickedID === 'equal') {
+            equalsPress();
+        } else { 
+            equalRepeat = false;
+        }
+        
         if (clickedClass === 'number') {
             numberPress(clickedID);
         }
         if (clickedClass === "operator") {
             operatorPress(clickedID);
         }
-        if (clickedID === 'equal') {
-            equalsPress();
-        }
+    
         if (clickedID === 'clear') {
             clearPress();
         }
